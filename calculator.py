50481b657391825671e6494026b230c451c87f65
wendell91097 <wendell91097@gmail.com>
2023-02-16 11:28:17 -0500

    first commit

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def calcArea(length, width):
+    """
+    For two parameters for sides of a rectangle, length and width, it returns the area for the rectangle.
+    """
+    return(length * width)
+
+def circle_perimeter(radius):
+    """
+    For one parameter, radius, it calculates the circumference of the circle. (diameter/2) returns the same result.
+    """
+    return (6.28*radius)
---
+++ module.py
@@ -0,0 +1,3 @@
+def printName(name):
+    return (f"Hello Mr/Ms {name}...we've been waiting for you!")
+    
